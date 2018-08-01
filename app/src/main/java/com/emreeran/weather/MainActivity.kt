f27fe7f798f19e9cffa7589d3dda8424e77c5b29
Emre Eran <emre.eran@gmail.com>
2018-08-02 02:13:53 +0900

    Overridden fragment injector and lifecycle getters; navigate up method

--- app/src/main/java/com/emreeran/weather/MainActivity.kt
+++ app/src/main/java/com/emreeran/weather/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.emreeran.weather
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import androidx.navigation.findNavController
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.HasSupportFragmentInjector
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LifecycleOwner, HasSupportFragmentInjector {
+
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
+
+    private val lifecycleRegistry = LifecycleRegistry(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
     }
+
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> = dispatchingAndroidInjector
+
+    override fun getLifecycle(): Lifecycle = lifecycleRegistry
+
+    override fun onNavigateUp(): Boolean = findNavController(R.id.main_nav_host).navigateUp()
 }

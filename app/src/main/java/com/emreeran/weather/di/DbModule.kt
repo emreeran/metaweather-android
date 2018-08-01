3dca4f7917c20f2441cde717271b5a2f961814d8
Emre Eran <emre.eran@gmail.com>
2018-08-02 02:09:09 +0900

    Created db dagger module

---
+++ app/src/main/java/com/emreeran/weather/di/DbModule.kt
@@ -0,0 +1,26 @@
+package com.emreeran.weather.di
+
+import android.app.Application
+import androidx.room.Room
+import com.emreeran.weather.db.WeatherDb
+import com.emreeran.weather.db.dao.LocationDao
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+/**
+ * Created by Emre Eran on 2.08.2018.
+ */
+@Module
+class DbModule {
+
+    @Singleton
+    @Provides
+    fun provideDb(app: Application, databaseInfo: DatabaseInfo): WeatherDb =
+            Room.databaseBuilder(app, WeatherDb::class.java, databaseInfo.name).build()
+
+
+    @Singleton
+    @Provides
+    fun provideLocationDao(db: WeatherDb): LocationDao = db.locationDao()
+}

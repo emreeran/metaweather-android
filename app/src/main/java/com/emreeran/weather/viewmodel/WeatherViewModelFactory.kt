fd65600348a6d33bbd4d3a375eca24fa48c0fba4
Emre Eran <emre.eran@gmail.com>
2018-08-03 03:15:03 +0900

    Created view model factory

---
+++ app/src/main/java/com/emreeran/weather/viewmodel/WeatherViewModelFactory.kt
@@ -0,0 +1,28 @@
+package com.emreeran.weather.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+
+/**
+ * Created by Emre Eran on 3.08.2018.
+ */
+@Singleton
+class WeatherViewModelFactory @Inject constructor(
+        private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        val creator = creators[modelClass] ?: creators.entries.firstOrNull {
+            modelClass.isAssignableFrom(it.key)
+        }?.value ?: throw IllegalArgumentException("unknown model class $modelClass")
+        try {
+            @Suppress("UNCHECKED_CAST")
+            return creator.get() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+}

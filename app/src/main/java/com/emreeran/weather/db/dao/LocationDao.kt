e55d9efd7ebd377063c4362c6c6d126ac25740a1
Emre Eran <emre.eran@gmail.com>
2018-08-03 01:50:35 +0900

    Added insert and query methods

--- app/src/main/java/com/emreeran/weather/db/dao/LocationDao.kt
+++ app/src/main/java/com/emreeran/weather/db/dao/LocationDao.kt
@@ -1,8 +1,10 @@
 package com.emreeran.weather.db.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.emreeran.weather.db.entity.Location
 
 /**
@@ -10,6 +12,19 @@ import com.emreeran.weather.db.entity.Location
  */
 @Dao
 interface LocationDao {
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(location: Location)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertIfNotExists(location: Location)
+
+    /**
+     * Replacing location will update created at value
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(locations: List<Location>)
+
+    @Query("SELECT * FROM locations ORDER BY distance ASC LIMIT 1")
+    fun getNearestLocationAsLiveData(): LiveData<Location>
 }

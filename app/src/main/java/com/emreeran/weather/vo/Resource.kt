e9b2a2a0ef5d82f0d7da8ae1edfb6b898baccdec
Emre Eran <emre.eran@gmail.com>
2018-08-03 01:50:13 +0900

    Created Resource vo

---
+++ app/src/main/java/com/emreeran/weather/vo/Resource.kt
@@ -0,0 +1,26 @@
+package com.emreeran.weather.vo
+
+/**
+ * Created by Emre Eran on 2.08.2018.
+ */
+data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
+    companion object {
+        fun <T> success(data: T?): Resource<T> {
+            return Resource(Status.SUCCESS, data, null)
+        }
+
+        fun <T> error(msg: String, data: T?): Resource<T> {
+            return Resource(Status.ERROR, data, msg)
+        }
+
+        fun <T> loading(data: T?): Resource<T> {
+            return Resource(Status.LOADING, data, null)
+        }
+    }
+}
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}

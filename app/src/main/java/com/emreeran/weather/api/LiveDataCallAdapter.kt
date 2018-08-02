f9c75da771b6a39ee3fb559d64d802679b6d5e38
Emre Eran <emre.eran@gmail.com>
2018-08-03 01:47:24 +0900

    Created adapter for live data responses, handling queueing calls and mapping to ApiResponse LiveData

---
+++ app/src/main/java/com/emreeran/weather/api/LiveDataCallAdapter.kt
@@ -0,0 +1,40 @@
+package com.emreeran.weather.api
+
+import androidx.lifecycle.LiveData
+import com.emreeran.weather.api.vo.ApiResponse
+import retrofit2.Call
+import retrofit2.CallAdapter
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.reflect.Type
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * Created by Emre Eran on 3.08.2018.
+ */
+class LiveDataCallAdapter<R>(private val responseType: Type) :
+        CallAdapter<R, LiveData<ApiResponse<R>>> {
+
+    override fun adapt(call: Call<R>?): LiveData<ApiResponse<R>> {
+        return object : LiveData<ApiResponse<R>>() {
+            private var started = AtomicBoolean(false)
+
+            override fun onActive() {
+                super.onActive()
+                if (started.compareAndSet(false, true)) {
+                    call?.enqueue(object : Callback<R> {
+                        override fun onResponse(call: Call<R>, response: Response<R>) {
+                            postValue(ApiResponse.create(response))
+                        }
+
+                        override fun onFailure(call: Call<R>, throwable: Throwable) {
+                            postValue(ApiResponse.create(throwable))
+                        }
+                    })
+                }
+            }
+        }
+    }
+
+    override fun responseType() = responseType
+}

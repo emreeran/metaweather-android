dcde4991eb4b07384de1c15279cc51c61115f516
Emre Eran <emre.eran@gmail.com>
2018-08-03 01:48:33 +0900

    Provided api and db info configs

--- app/src/main/java/com/emreeran/weather/di/ConfigModule.kt
+++ app/src/main/java/com/emreeran/weather/di/ConfigModule.kt
@@ -1,14 +1,22 @@
 package com.emreeran.weather.di
 
 import dagger.Module
+import dagger.Provides
 
 /**
  * Created by Emre Eran on 2.08.2018.
  */
 @Module
 class ConfigModule {
+
+    @Provides
     fun provideMetaWeatherApiInfo(): MetaWeatherApiInfo {
         return MetaWeatherApiInfo
+    }
+
+    @Provides
+    fun provideDatabaseInfo(): DatabaseInfo {
+        return DatabaseInfo
     }
 }
 

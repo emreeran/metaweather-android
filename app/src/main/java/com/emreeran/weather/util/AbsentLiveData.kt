1085a45a25d29a4f64b7655bb20e48106f6888c8
Emre Eran <emre.eran@gmail.com>
2018-08-03 03:15:20 +0900

    Created null live data util

---
+++ app/src/main/java/com/emreeran/weather/util/AbsentLiveData.kt
@@ -0,0 +1,19 @@
+package com.emreeran.weather.util
+
+import androidx.lifecycle.LiveData
+
+/**
+ * Created by Emre Eran on 3.08.2018.
+ */
+class AbsentLiveData<T : Any?> private constructor() : LiveData<T>() {
+    init {
+        // use post instead of set since this can be created on any thread
+        postValue(null)
+    }
+
+    companion object {
+        fun <T> create(): LiveData<T> {
+            return AbsentLiveData()
+        }
+    }
+}

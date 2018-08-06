51a633cbbfdef1c6e7c105b5e8e264c44a8810c9
Emre Eran <emre.eran@gmail.com>
2018-08-06 14:14:10 +0900

    Created ForecastDao

---
+++ app/src/main/java/com/emreeran/weather/db/dao/ForecastDao.kt
@@ -0,0 +1,35 @@
+package com.emreeran.weather.db.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.emreeran.weather.db.entity.Forecast
+import com.emreeran.weather.db.entity.ForecastDay
+import com.emreeran.weather.vo.ForecastWithItems
+
+/**
+ * Created by Emre Eran on 3.08.2018.
+ */
+@Dao
+abstract class ForecastDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insert(forecast: Forecast): Long
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insertForecastDays(items: List<ForecastDay>)
+
+    @Query("SELECT * FROM forecast_days WHERE forecastId = :id AND date >= :date ORDER BY date ASC")
+    abstract fun listForecastDaysByForecastIdAfterDate(id: Int, date: Long): LiveData<List<ForecastDay>>
+
+    @Query("SELECT * FROM forecasts WHERE locationId = :locationId ORDER BY time DESC LIMIT 1")
+    abstract fun getLatestForecastByLocationId(locationId: Int): Forecast
+
+    @Query("SELECT * FROM forecast_days WHERE forecastId = :forecastId ORDER BY date ASC")
+    abstract fun listForecastDaysByForecastId(forecastId: Int): List<ForecastDay>
+
+    @Transaction
+    open fun getLatestForecastByLocationWithDays(locationId: Int): ForecastWithItems {
+        val forecast = getLatestForecastByLocationId(locationId)
+        val items = listForecastDaysByForecastId(forecast.id)
+        return ForecastWithItems(forecast, items)
+    }
+}

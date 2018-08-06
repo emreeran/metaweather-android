afb77d0dd3b0deffda91245039988eaf8d4be59e
Emre Eran <emre.eran@gmail.com>
2018-08-06 14:14:38 +0900

    Added Forecast, ForecastDay entities and ForecastDao to WeatherDB

--- app/src/main/java/com/emreeran/weather/db/WeatherDb.kt
+++ app/src/main/java/com/emreeran/weather/db/WeatherDb.kt
@@ -3,7 +3,10 @@ package com.emreeran.weather.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.emreeran.weather.db.dao.ForecastDao
 import com.emreeran.weather.db.dao.LocationDao
+import com.emreeran.weather.db.entity.Forecast
+import com.emreeran.weather.db.entity.ForecastDay
 import com.emreeran.weather.db.entity.Location
 
 /**
@@ -11,11 +14,15 @@ import com.emreeran.weather.db.entity.Location
  */
 @Database(
         entities = [
-            Location::class
+            Location::class,
+            Forecast::class,
+            ForecastDay::class
         ],
         version = 1
 )
 @TypeConverters(Converters::class)
 abstract class WeatherDb : RoomDatabase() {
     abstract fun locationDao(): LocationDao
+
+    abstract fun forecastDao(): ForecastDao
 }

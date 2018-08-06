8dc9c0bb0ddae6ae0e7f26491f4cf953562b5755
Emre Eran <emre.eran@gmail.com>
2018-08-06 14:15:37 +0900

    Added logging interceptor and TimeZone type adapter to Retrofit api

--- app/src/main/java/com/emreeran/weather/di/ApiModule.kt
+++ app/src/main/java/com/emreeran/weather/di/ApiModule.kt
@@ -1,16 +1,17 @@
 package com.emreeran.weather.di
 
+import com.emreeran.weather.BuildConfig
 import com.emreeran.weather.api.LiveDataCallAdapterFactory
 import com.emreeran.weather.api.MetaWeatherService
-import com.emreeran.weather.api.vo.Coordinates
-import com.emreeran.weather.api.vo.CoordinatesDeserializer
-import com.emreeran.weather.api.vo.LocationType
-import com.emreeran.weather.api.vo.LocationTypeDeserializer
+import com.emreeran.weather.api.vo.*
 import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 import javax.inject.Singleton
 
 /**
@@ -22,15 +23,27 @@ class ApiModule {
     @Singleton
     @Provides
     fun provideMetaWeatherService(info: MetaWeatherApiInfo): MetaWeatherService {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        } else {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.NONE
+        }
+
+        val client: OkHttpClient = OkHttpClient.Builder()
+                .addInterceptor(httpLoggingInterceptor).build()
+
         val gsonBuilder = GsonBuilder()
         gsonBuilder.registerTypeAdapter(LocationType::class.java, LocationTypeDeserializer())
         gsonBuilder.registerTypeAdapter(Coordinates::class.java, CoordinatesDeserializer())
+        gsonBuilder.registerTypeAdapter(TimeZone::class.java, TimezoneDeserializer())
         val gson = gsonBuilder.create()
 
         return Retrofit.Builder()
                 .baseUrl(info.url)
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .addCallAdapterFactory(LiveDataCallAdapterFactory())
+                .client(client)
                 .build()
                 .create(MetaWeatherService::class.java)
     }

3238c4ed457d318ec2b4ba1a61d77ccd69cd6ad7
Emre Eran <emre.eran@gmail.com>
2018-08-06 14:13:27 +0900

    Added getForecastByLocationId endpoint to api service

--- app/src/main/java/com/emreeran/weather/api/MetaWeatherService.kt
+++ app/src/main/java/com/emreeran/weather/api/MetaWeatherService.kt
@@ -2,9 +2,11 @@ package com.emreeran.weather.api
 
 import androidx.lifecycle.LiveData
 import com.emreeran.weather.api.vo.ApiResponse
+import com.emreeran.weather.api.vo.ForecastResponse
 import com.emreeran.weather.api.vo.LocationSearchResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 /**
@@ -16,4 +18,7 @@ interface MetaWeatherService {
 
     @GET("api/location/search/")
     fun searchLocationByCoordinates(@Query("lattlong") latLong: String): LiveData<ApiResponse<List<LocationSearchResponse>>>
+
+    @GET("api/location/{id}/")
+    fun getForecastByLocationId(@Path("id") id: Int): LiveData<ApiResponse<ForecastResponse>>
 }

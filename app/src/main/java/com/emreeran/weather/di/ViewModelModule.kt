f41294ddac35494e24b74f64a7c0a27469c1c647
Emre Eran <emre.eran@gmail.com>
2018-08-08 23:28:18 +0900

    Bound LocationSearchViewModel

--- app/src/main/java/com/emreeran/weather/di/ViewModelModule.kt
+++ app/src/main/java/com/emreeran/weather/di/ViewModelModule.kt
@@ -3,6 +3,7 @@ package com.emreeran.weather.di
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.emreeran.weather.ui.ForecastViewModel
+import com.emreeran.weather.ui.LocationSearchViewModel
 import com.emreeran.weather.viewmodel.WeatherViewModelFactory
 import dagger.Binds
 import dagger.Module
@@ -18,6 +19,11 @@ abstract class ViewModelModule {
     @IntoMap
     @ViewModelKey(ForecastViewModel::class)
     abstract fun bindForecastViewModel(forecastViewModel: ForecastViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(LocationSearchViewModel::class)
+    abstract fun bindLocationSearchViewModel(locationSearchViewModel: LocationSearchViewModel): ViewModel
 
     @Binds
     abstract fun bindViewModelFactory(factory: WeatherViewModelFactory): ViewModelProvider.Factory

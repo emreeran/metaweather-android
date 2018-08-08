2d043cf00f57a442414f8116c3b7b448ade0369e
Emre Eran <emre.eran@gmail.com>
2018-08-08 23:12:21 +0900

    Implemented searchLocationsByName method

--- app/src/main/java/com/emreeran/weather/repository/LocationRepository.kt
+++ app/src/main/java/com/emreeran/weather/repository/LocationRepository.kt
@@ -2,15 +2,19 @@ package com.emreeran.weather.repository
 
 import android.text.format.DateUtils
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.emreeran.weather.AppExecutors
 import com.emreeran.weather.api.MetaWeatherService
 import com.emreeran.weather.api.vo.LocationSearchResponse
 import com.emreeran.weather.db.dao.LocationDao
 import com.emreeran.weather.db.entity.Location
+import com.emreeran.weather.db.entity.LocationQuery
+import com.emreeran.weather.vo.LocationQueryWithLocations
 import com.emreeran.weather.vo.Resource
 import java.util.*
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.collections.ArrayList
 
 /**
  * Created by Emre Eran on 2.08.2018.
@@ -25,33 +29,7 @@ class LocationRepository @Inject constructor(
         return object : NetworkBoundResource<Location, List<LocationSearchResponse>>(appExecutors) {
 
             override fun saveCallResult(body: List<LocationSearchResponse>) {
-                val locations = ArrayList<Location>()
-
-                for (item in body) {
-                    var parent: Location? = null
-                    item.parent?.let { it ->
-                        parent = Location(
-                                woeId = it.woeId,
-                                name = it.title,
-                                type = it.locationType,
-                                coordinates = it.latLong
-                        )
-                        locationDao.insertIfNotExists(parent!!)
-                    }
-
-                    val location = Location(
-                            woeId = item.woeId,
-                            parentId = parent?.woeId,
-                            name = item.title,
-                            type = item.locationType,
-                            coordinates = item.latLong,
-                            distance = item.distance
-                    )
-
-                    locations.add(location)
-                }
-
-                locationDao.insertAll(locations)
+                saveLocations(body)
             }
 
             override fun shouldFetch(data: Location?): Boolean {
@@ -68,5 +46,69 @@ class LocationRepository @Inject constructor(
             override fun createCall() = weatherService.searchLocationByCoordinates("$lat,$long")
 
         }.asLiveData()
+    }
+
+    fun searchLocationsByName(queryString: String): LiveData<Resource<LocationQueryWithLocations>> {
+        return object : NetworkBoundResource<LocationQueryWithLocations, List<LocationSearchResponse>>(appExecutors) {
+            override fun saveCallResult(body: List<LocationSearchResponse>) {
+                val ids = saveLocations(body)
+                val query = LocationQuery(queryString, ids)
+                locationDao.insertQuery(query)
+            }
+
+            override fun shouldFetch(data: LocationQueryWithLocations?): Boolean {
+                data?.query?.let {
+                    val hourAgo = Date()
+                    hourAgo.time = hourAgo.time - DateUtils.HOUR_IN_MILLIS
+                    return it.createdAt.before(hourAgo)
+                }
+                return true
+            }
+
+            override fun loadFromDb(): LiveData<LocationQueryWithLocations> {
+                val result = MutableLiveData<LocationQueryWithLocations>()
+                appExecutors.diskIO().execute {
+                    val data = locationDao.listQueryLocations(queryString)
+                    result.postValue(data)
+                }
+                return result
+            }
+
+            override fun createCall() = weatherService.searchLocationByName(queryString)
+
+        }.asLiveData()
+    }
+
+    private fun saveLocations(locationsResponse: List<LocationSearchResponse>): List<Int> {
+        val locations = ArrayList<Location>()
+        val ids = ArrayList<Int>()
+
+        for (item in locationsResponse) {
+            var parent: Location? = null
+            item.parent?.let { it ->
+                parent = Location(
+                        woeId = it.woeId,
+                        name = it.title,
+                        type = it.locationType,
+                        coordinates = it.latLong
+                )
+                locationDao.insertIfNotExists(parent!!)
+            }
+
+            val location = Location(
+                    woeId = item.woeId,
+                    parentId = parent?.woeId,
+                    name = item.title,
+                    type = item.locationType,
+                    coordinates = item.latLong,
+                    distance = item.distance
+            )
+
+            locations.add(location)
+            ids.add(location.woeId)
+        }
+
+        locationDao.insertAll(locations)
+        return ids
     }
 }

0e9655d3afa207027d0e9b7615eaaa38510e095c
Emre Eran <emre.eran@gmail.com>
2018-08-08 23:12:04 +0900

    Implemented LocationSearch related dao methods

--- app/src/main/java/com/emreeran/weather/db/dao/LocationDao.kt
+++ app/src/main/java/com/emreeran/weather/db/dao/LocationDao.kt
@@ -1,30 +1,49 @@
 package com.emreeran.weather.db.dao
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.emreeran.weather.db.entity.Location
+import com.emreeran.weather.db.entity.LocationQuery
+import com.emreeran.weather.vo.LocationQueryWithLocations
 
 /**
  * Created by Emre Eran on 2.08.2018.
  */
 @Dao
-interface LocationDao {
+abstract class LocationDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(location: Location)
+    abstract fun insert(location: Location)
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insertIfNotExists(location: Location)
+    abstract fun insertIfNotExists(location: Location)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insertQuery(query: LocationQuery)
 
     /**
      * Replacing location will update created at value
      */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertAll(locations: List<Location>)
+    abstract fun insertAll(locations: List<Location>)
+
+    @Query("SELECT * FROM locations WHERE woeId IN (:ids)")
+    abstract fun listLocationsByIds(ids: List<Int>): List<Location>
 
     @Query("SELECT * FROM locations ORDER BY distance ASC LIMIT 1")
-    fun getNearestLocationAsLiveData(): LiveData<Location>
+    abstract fun getNearestLocationAsLiveData(): LiveData<Location>
+
+    @Query("SELECT * FROM location_query WHERE queryString = :queryString")
+    abstract fun findLocationQuery(queryString: String): LocationQuery?
+
+    @Transaction
+    open fun listQueryLocations(queryString: String): LocationQueryWithLocations {
+        val query = findLocationQuery(queryString)
+
+        query?.let {
+            return LocationQueryWithLocations(query, listLocationsByIds(it.locationIds))
+        }
+
+        return LocationQueryWithLocations(query)
+    }
 }

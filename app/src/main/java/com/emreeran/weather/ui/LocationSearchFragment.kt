55e1d4dfa098a5d101ac230b7b5f831de579a4d5
Emre Eran <emre.eran@gmail.com>
2018-08-08 23:29:35 +0900

    Added view model and added observer for location query

--- app/src/main/java/com/emreeran/weather/ui/LocationSearchFragment.kt
+++ app/src/main/java/com/emreeran/weather/ui/LocationSearchFragment.kt
@@ -6,17 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.emreeran.weather.R
 import com.emreeran.weather.databinding.LocationSearchFragmentBinding
 import com.emreeran.weather.di.Injectable
 import com.emreeran.weather.util.autoCleared
+import timber.log.Timber
+import javax.inject.Inject
 
 /**
  * Created by Emre Eran on 8.08.2018.
  */
 class LocationSearchFragment : Fragment(), Injectable {
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
     var binding by autoCleared<LocationSearchFragmentBinding>()
+
+    lateinit var locationSearchViewModel: LocationSearchViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil
@@ -26,5 +36,12 @@ class LocationSearchFragment : Fragment(), Injectable {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        locationSearchViewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(LocationSearchViewModel::class.java)
+
+        val query = locationSearchViewModel.locationQuery
+        query.observe(this, Observer {
+            Timber.i(it.data.toString())
+        })
     }
 }

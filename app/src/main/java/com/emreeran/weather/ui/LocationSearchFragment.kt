c0374699702a2f51aac1d6a05c8e38c95ee16a0f
Emre Eran <emre.eran@gmail.com>
2018-08-11 19:01:06 +0900

    Implemented location list adapter

--- app/src/main/java/com/emreeran/weather/ui/LocationSearchFragment.kt
+++ app/src/main/java/com/emreeran/weather/ui/LocationSearchFragment.kt
@@ -1,22 +1,29 @@
 package com.emreeran.weather.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.os.Handler
+import android.os.IBinder
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingComponent
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import com.emreeran.weather.AppExecutors
+import com.emreeran.weather.MainActivity
 import com.emreeran.weather.R
+import com.emreeran.weather.binding.FragmentDataBindingComponent
 import com.emreeran.weather.databinding.LocationSearchFragmentBinding
 import com.emreeran.weather.di.Injectable
 import com.emreeran.weather.util.autoCleared
-import timber.log.Timber
 import javax.inject.Inject
 
 /**
@@ -27,17 +34,29 @@ class LocationSearchFragment : Fragment(), Injectable {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
+    @Inject
+    lateinit var appExecutors: AppExecutors
+
     lateinit var locationSearchViewModel: LocationSearchViewModel
 
     var binding by autoCleared<LocationSearchFragmentBinding>()
 
+    private var adapter by autoCleared<LocationAdapter>()
     private var searchKeyword = ""
     private var lastInput: Long = 0
     private val minimumSearchCharLength = 3
     private val inputHandler = Handler()
     private val inputWaitDelay: Long = 1000
 
+    private val dataBindingComponent: DataBindingComponent = FragmentDataBindingComponent(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(false)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        (activity as MainActivity).supportActionBar?.hide()
         binding = DataBindingUtil
                 .inflate(inflater, R.layout.location_search_fragment, container, false)
         return binding.root
@@ -49,11 +68,23 @@ class LocationSearchFragment : Fragment(), Injectable {
                 .get(LocationSearchViewModel::class.java)
 
         val query = locationSearchViewModel.locationQuery
-        query.observe(this, Observer {
-            Timber.i(it.data.toString())
+        query.observe(this, Observer { resource ->
+            resource.data?.let {
+                adapter.submitList(it.locations)
+            } ?: kotlin.run {
+                adapter.submitList(emptyList())
+            }
         })
 
         binding.search.addTextChangedListener(onSearchTextChanged)
+
+        adapter = LocationAdapter(dataBindingComponent, appExecutors) {
+            dismissKeyboard(binding.root.windowToken)
+            val action = LocationSearchFragmentDirections.showForecast(null, null)
+            action.setLocationId(it.woeId)
+            findNavController(this).navigate(action)
+        }
+        binding.resultList.adapter = adapter
     }
 
     private val searchIfInputFinished = Runnable {
@@ -80,5 +111,10 @@ class LocationSearchFragment : Fragment(), Injectable {
                 }
             }
         }
+    }
+
+    private fun dismissKeyboard(windowToken: IBinder) {
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        imm?.hideSoftInputFromWindow(windowToken, 0)
     }
 }

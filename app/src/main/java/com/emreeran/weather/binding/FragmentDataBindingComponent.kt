2e0c5a09400b42707c99454dc0d955a6d61078d5
Emre Eran <emre.eran@gmail.com>
2018-08-11 18:56:03 +0900

    Created FragmentDataBindingComponent

---
+++ app/src/main/java/com/emreeran/weather/binding/FragmentDataBindingComponent.kt
@@ -0,0 +1,9 @@
+package com.emreeran.weather.binding
+
+import androidx.databinding.DataBindingComponent
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by Emre Eran on 11.08.2018.
+ */
+class FragmentDataBindingComponent(fragment: Fragment) : DataBindingComponent

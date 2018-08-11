54294d29651bb13c57193e0ccf5751b4274f7ef2
Emre Eran <emre.eran@gmail.com>
2018-08-11 14:29:20 +0900

    Made getLatestForecastByLocationId query result nullable

--- app/src/main/java/com/emreeran/weather/db/dao/ForecastDao.kt
+++ app/src/main/java/com/emreeran/weather/db/dao/ForecastDao.kt
@@ -21,15 +21,18 @@ abstract class ForecastDao {
     abstract fun listForecastDaysByForecastIdAfterDate(id: Int, date: Long): LiveData<List<ForecastDay>>
 
     @Query("SELECT * FROM forecasts WHERE locationId = :locationId ORDER BY time DESC LIMIT 1")
-    abstract fun getLatestForecastByLocationId(locationId: Int): Forecast
+    abstract fun getLatestForecastByLocationId(locationId: Int): Forecast?
 
     @Query("SELECT * FROM forecast_days WHERE forecastId = :forecastId ORDER BY date ASC")
     abstract fun listForecastDaysByForecastId(forecastId: Int): List<ForecastDay>
 
     @Transaction
-    open fun getLatestForecastByLocationWithDays(locationId: Int): ForecastWithItems {
+    open fun getLatestForecastByLocationWithDays(locationId: Int): ForecastWithItems? {
         val forecast = getLatestForecastByLocationId(locationId)
-        val items = listForecastDaysByForecastId(forecast.id)
-        return ForecastWithItems(forecast, items)
+        forecast?.let {
+            val items = listForecastDaysByForecastId(it.id)
+            return ForecastWithItems(forecast, items)
+        }
+        return null
     }
 }
